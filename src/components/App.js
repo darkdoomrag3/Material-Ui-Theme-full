8bb655302c734baea8999dcdf9ab16ad3599a1bb
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-20 16:04:33 +0430

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './ui/Header';
+
+
+
+function App() {
+  return (
+    <>
+
+      <Header />
+ Hello
+    </>
+  );
+}
+
+export default App;
+
+
+
+///// 2 13
